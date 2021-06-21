19fe87ccc1c0f48f1ea8576b5797c038ee2cfbd3
Lee Ji Heok <olzlgur@naver.com>
2021-06-21 23:30:55 +0900

    2021-06-21

---
+++ homework1/p2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework1
+{
+    class p2
+    {
+        public static void Main()
+        {
+            int a;
+            int b;
+            a = 2;
+            b = 3;
+            a = b;
+            Console.WriteLine(a);
+        }
+
+    }
+}
+
+// 결과
+// 3
