1bd35b71435a579289176d62f8d16ee1161a0cbf
Lee Ji Heok <olzlgur@naver.com>
2021-07-13 22:02:29 +0900

    2021-07-13

---
+++ homework2/p1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace homework1
+{
+    class p1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// 결과
+// Hello World! 
